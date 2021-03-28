866fe7193e232786ff0f739ba634649e14c3ab2c
rmwsb <gdx142905@student.wsb.gda.pl>
2021-03-28 23:41:19 +0200

    zadanie 3

--- src/pl/wsb/Main.java
+++ src/pl/wsb/Main.java
@@ -21,6 +21,27 @@ public class Main {
         Phone phone2 = new Phone("Samsung", "Galaxy S20", 6.2, 0.163);
 
         System.out.println(animal2.name);
+        System.out.println("\n----------------\n");
 
+        Animal animalWithMaxWeight = animal1.weight > animal2.weight ? animal1 : animal2;
+        StringBuilder animalWithMaxWeightDescription = new StringBuilder()
+                .append("Heaviest animal: ")
+                .append(animalWithMaxWeight.name)
+                .append(", weight: ")
+                .append(animalWithMaxWeight.weight)
+                .append(" kg");
+        System.out.println(animalWithMaxWeightDescription);
+        System.out.println("\n----------------\n");
+
+        Human humanWithMaxAge = human1.age > human2.age ? human1 : human2;
+        StringBuilder humanWithMaxAgeDescription = new StringBuilder()
+                .append("Oldest human: ")
+                .append(humanWithMaxAge.firstName)
+                .append(" ")
+                .append(humanWithMaxAge.lastName)
+                .append(", age: ")
+                .append(humanWithMaxAge.age);
+        System.out.println(humanWithMaxAgeDescription);
+        System.out.println("\n----------------\n");
     }
 }
