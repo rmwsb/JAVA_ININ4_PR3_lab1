827170e7a815ec77422797da70fdd339f857de48
rmwsb <gdx142905@student.wsb.gda.pl>
2021-03-28 22:59:49 +0200

    zadanie 1

---
+++ src/pl/wsb/Main.java
@@ -0,0 +1,14 @@
+package pl.wsb;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println(args);
+
+        Date d = new Date();
+        System.out.println(d.toString());
+    }
+}
